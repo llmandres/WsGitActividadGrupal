308e61f47d27fab05a2e995f3eee645a2de0ac2f
yolmi <andresgarciasampayo@gmail.com>
2024-02-15 13:52:12 +0100

    Creacion de objetos en el main

--- Actividad10_Git_Grupal/src/gitactividad/MainGit.java
+++ Actividad10_Git_Grupal/src/gitactividad/MainGit.java
@@ -1,8 +1,17 @@
 package gitactividad;
 
+import java.util.Scanner;
+
 public class MainGit {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Usuario[] user1 = new Usuario[3];
+		System.out.println("Cuantas valoraciones va a introducir en general?");
+		int arrayTamanio = scan.nextInt();
+		user1[0] = new Usuario("2321f", "Paco", new double[arrayTamanio] );
+		user1[1] = new Usuario("1WDSF", "JUAN", new double[] {1.0, 3.4, 5.2, 6.2, 10.0} );
+		user1[2] = new Usuario("1112D", "Antonio", new double[arrayTamanio] );
 		
 	}
 
