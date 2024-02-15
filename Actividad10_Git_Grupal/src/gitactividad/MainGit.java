053d7636fc4befbc6563e4370c7d5e0929c13b4d
MarkDelsius <marcosdelsaz2005@gmail.com>
2024-02-15 13:55:42 +0100

    Creacion de recogida de valores del array 'valoraciones'

--- Actividad10_Git_Grupal/src/gitactividad/MainGit.java
+++ Actividad10_Git_Grupal/src/gitactividad/MainGit.java
@@ -13,6 +13,26 @@ public class MainGit {
 		user1[1] = new Usuario("1WDSF", "JUAN", new double[] {1.0, 3.4, 5.2, 6.2, 10.0} );
 		user1[2] = new Usuario("1112D", "Antonio", new double[arrayTamanio] );
 		
+		System.out.println("Valoraciones Primer usuario");
+		for (int j = 0;j< arrayTamanio;j++) {
+			double valoracion = 0;
+			do {
+			System.out.println("Introduce la valoracion numero " + (j+1));
+			valoracion = scan.nextDouble();
+			}while((valoracion>10) || (valoracion<0) );
+			user1[0].valoraciones[j] = valoracion;
+		}
+		
+		System.out.println("Valoraciones Segundo usuario");
+		for (int j = 0;j< arrayTamanio;j++) {
+			double valoracion = 0;
+			do {
+			System.out.println("Introduce la valoracion del Usuario 3 | numero " + (j+1));
+			valoracion = scan.nextDouble();
+			}while((valoracion>10) || (valoracion<0) ); 
+			user1[2].valoraciones[j] = valoracion;
+		}
+
 	}
 
 }
